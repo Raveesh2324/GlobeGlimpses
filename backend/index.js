ef518b5a4dce1b49128818488fd2cd40a17a97ce
Raveesh Goyal <raveeshgoyal2504@gmail.com>
2023-10-10 14:57:01 +0530

    initialise the project, connect with the mongodb, create the folder structure of frontend, add CSS and and implement baisc Home Page

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://raveeshgoyal:123raveesh@cluster0.vguizun.mongodb.net/";
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
